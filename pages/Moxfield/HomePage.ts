c6b62ed263f0e3132a80efa3840dfe2cb8f95e98
Antileptons <garzaestradaleonardo@gmail.com>
2024-10-11 13:51:18 -0600

    Moxfield Updated

--- pages/Moxfield/HomePage.ts
+++ pages/Moxfield/HomePage.ts
@@ -3,16 +3,23 @@ import {Page,Locator} from '@playwright/test'
 export default class MoxHomePage {
 
     readonly page: Page;
+    readonly exploreButton: Locator;
+    readonly comPrecButton: Locator;
 
     constructor (page: Page) {
         this.page = page;
-
+        this.exploreButton = page.locator('xpath=//*[@id="mainmain-dropdown"]/ul[1]/li[2]/a');
+        this.comPrecButton = page.locator('xpath=/html/body/div[2]/div/div/div[1]/a[2]/span[2]/span')
     }
     
     async goto () {
         await this.page.goto('https://www.moxfield.com/');
     }
 
+    async clickoncprecons () {
+        await this.exploreButton.click();
+        await this.comPrecButton.click();
+    }
 
 
 }
--- tests/Moxfield/checkhomescreen.spec.ts
+++ tests/Moxfield/checkhomescreen.spec.ts
@@ -1,9 +1,15 @@
 import { test,expect } from "../../Fixtures";
+import MoxHomePage from "../../pages/Moxfield/HomePage";
 
 test('Verify Moxfield Home Screen',async ({moxHomePage}) => {
 
     await expect(moxHomePage.page).toHaveURL('https://www.moxfield.com/');
 
+});
 
+test('Verify Commander Precon Page',async ({moxHomePage}) => {
+
+    await moxHomePage.clickoncprecons();
+    await expect(moxHomePage.page).toHaveURL('https://www.moxfield.com/decks/public?format=commanderPrecons')
 
 });
